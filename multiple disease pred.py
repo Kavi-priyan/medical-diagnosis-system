bc85182523cc2877d4fb27fa3bea898998e0b179
Kavi-priyan <131338635+Kavi-priyan@users.noreply.github.com>
2023-10-19 20:52:52 +0530

    Add files via upload

---
+++ multiple disease pred.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 22 11:53:51 2022
+
+@author: siddhardhan
+"""
+
+import pickle
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+
+# loading the saved models
+
+diabetes_model = pickle.load(open('C:/Users/rkchi/OneDrive/Desktop/Multiple Disease Prediction System/saved model/diabetes_model.sav','rb'))
+heart_model = pickle.load(open('C:/Users/rkchi/OneDrive/Desktop/Multiple Disease Prediction System/saved model/heart_model.sav','rb'))
+parkinson_model = pickle.load(open('C:/Users/rkchi/OneDrive/Desktop/Multiple Disease Prediction System/saved model/parkinson_model.sav','rb'))
+
+
+
+
+with st.sidebar:
+    
+    selected = option_menu('Multiple Disease Prediction System',
+                          
+                          ['Diabetes Prediction',
+                           'Heart Disease Prediction',
+                           'Parkinsons Prediction'],
+                        
+                          default_index=2)
+    
+    
+# Diabetes Prediction Page
+if (selected == 'Diabetes Prediction'):
+
+    
+    # page title
+    st.title('Diabetes Prediction using ML')
+
+if (selected == "Parkinsons Prediction"):
+    
+    # page title
+    st.title("Parkinson's Disease Prediction using ML")
+
+
+
+if (selected == 'Heart Disease Prediction'):
+    
+    # page title
+    st.title('Heart Disease Prediction using ML')
+    
+    
+    
